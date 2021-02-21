b41740ba59c5b5e08c15ceb4b703b434d1f28896
AkshajJain <akshajj2004@gmail.com>
2021-02-21 12:22:29 +0530

    Akshaj: removed redundant empty lines and reformatted code in gameUtilities.py

--- gameUtilities.py
+++ gameUtilities.py
@@ -1,30 +1,27 @@
 import sqlite3,  bz2
 
 class Serializer:
-
     def __init__( self, target ):
-        self.name  =  "Worlds/" + target + '.db'
-        self.conn  =  sqlite3.connect( self.name )
+        self.name = "Worlds/" + target + '.db'
+        self.conn = sqlite3.connect( self.name )
         c = self.conn.cursor()
         try:
-            ## Create Table
+            # Create Table
             c.execute( '''CREATE TABLE terrain(keys INTEGER NOT NULL PRIMARY KEY, list TEXT, local TEXT, entity TEXT)''' )
             self.conn.commit()
             c.execute( '''CREATE TABLE player(playername TEXT NOT NULL PRIMARY KEY, pickledplayer TEXT)''' )
             self.conn.commit()
-
         except Exception as e:
             pass
-    def __setitem__( self, key, t ):
 
+    def __setitem__( self, key, t ):
         c = self.conn.cursor()
         try:
-            ## Save string at new key location
+            # Save string at new key location
             c.execute( '''INSERT INTO terrain (keys, list, local) VALUES (?,?,?)''', ( key, bz2.compress( t[0] ), bz2.compress( t[1] ) ) )
             self.conn.commit()
-
         except Exception as e:
-            ## Update string at existing key
+            # Update string at existing key
             c.execute( 'UPDATE terrain SET list =?, local =?  WHERE keys=?', ( bz2.compress( t[0] ), bz2.compress( t[1] ), key ) )
             self.conn.commit()
 
@@ -35,7 +32,6 @@ class Serializer:
         c.execute('''SELECT local FROM terrain WHERE keys=?''', (key,))
         lo = c.fetchone()
         self.conn.commit()
-
         try:
             li = bz2.decompress( li[0] )
             lo = bz2.decompress( lo[0] )
@@ -45,7 +41,7 @@ class Serializer:
 
     def setEntity(self, key, li):
         c = self.conn.cursor()
-        ## Update string at existing key
+        # Update string at existing key
         c.execute('UPDATE terrain SET entity =?, WHERE keys=?', (bz2.compress(li), key))
         self.conn.commit()
 
@@ -60,25 +56,21 @@ class Serializer:
             return None
 
     def savePlayer( self, name, pickled ):
-
         c = self.conn.cursor()
         try:
-            ## Save pickledplayer at new playername
+            # Save pickledplayer at new playername
             c.execute( '''INSERT INTO player (playername, pickledplayer) VALUES (?,?)''', ( name, bz2.compress( pickled ) ) )
             self.conn.commit()
-
         except Exception as e:
-            ## Update pickledplayer at existing playername
+            # Update pickledplayer at existing playername
             c.execute( 'UPDATE player SET pickledplayer =?  WHERE playername=?', ( bz2.compress( pickled ), name ) )
             self.conn.commit()
 
     def loadPlayer( self, name ):
-
         c = self.conn.cursor()
         c.execute( '''SELECT pickledplayer FROM player WHERE playername=?''', ( name, ) )
         res = c.fetchone()
         self.conn.commit()
-
         try:
             return bz2.decompress( res[0] )
         except Exception as e:
