8642d831f08fa74af9405a39f60bb62773fd4f70
Aditya-A-garwal <aditya.agarwal030504@gmail.com>
2021-03-31 16:01:07 +0530

    Aditya: Started implementing the enumeration classes

--- tiles.py
+++ tiles.py
@@ -2,3 +2,10 @@ from constants import *
 
 class tiles( enum.Enum ):
     air = 0
+
+class items( enum.Enum ):
+    pass
+
+# liquids will be modifiers which will be
+# used as overlays on top of existing blocks
+# an empty slot can have a certain liquid level but no more
